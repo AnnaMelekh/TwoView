262f94a0269ea1e981a14854ad77514fbde1ab19
Anna <annasmelekhina@gmail.com>
2021-12-08 22:22:59 +0300

    Initial Commit

---
+++ TwoView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwoView
+//
+//  Created by Анна Мелехина on 08.12.2021.
+//  Copyright © 2021 Anna Melekhina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
